82592b9698afbf6065d32f6c2c0287a224916539
Anna Skawińska <a_skawinska@o2.pl>
2019-10-01 22:24:36 +0200

    Updates

---
+++ precache-manifest.c0e0bedb491d0b1d9fd5bb90f8b9e5d7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c2b5675fee25d912dfe440389347806",
+    "url": "/quango/index.html"
+  },
+  {
+    "revision": "b8e5f2c6ed04996e3d38",
+    "url": "/quango/static/css/main.18737572.chunk.css"
+  },
+  {
+    "revision": "4370fcb5aca94fff55c8",
+    "url": "/quango/static/js/2.e01b3d86.chunk.js"
+  },
+  {
+    "revision": "b8e5f2c6ed04996e3d38",
+    "url": "/quango/static/js/main.96680ea3.chunk.js"
+  },
+  {
+    "revision": "7e9b04bdbbd590014d0c",
+    "url": "/quango/static/js/runtime~main.3cfa9d0f.js"
+  },
+  {
+    "revision": "6228bdf37b3686c42aca486e1e15be7c",
+    "url": "/quango/static/media/Fat_pig.6228bdf3.webp"
+  },
+  {
+    "revision": "535835c00e48424a623cadce801676db",
+    "url": "/quango/static/media/pichu.535835c0.webp"
+  },
+  {
+    "revision": "0def0ed7e90c0a80d3260a742cae72ef",
+    "url": "/quango/static/media/pikachu.0def0ed7.png"
+  },
+  {
+    "revision": "0f8127d0617b130a6e9810f05e8be5ea",
+    "url": "/quango/static/media/red.0f8127d0.webp"
+  }
+]);
